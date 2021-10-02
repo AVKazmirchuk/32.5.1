62fee44833ddc1b5c8ea977b625043302c483c78
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-10-02 15:00:25 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
-#include <vector>
-#include <algorithm>
-#include <sstream>
 
-#include "../include/header.h"
 #include "nlohmann/json.hpp"
 
-int main()
-{
-    nlohmann::json moonshiners = {
-            {"country", "USSR"},
-            {"date", "1962"},
-            {"studio", "Mosfilm"},
-            {"scriptwriter", {"Konstantin Brovin", "Leonid Gaidai"}},
-            {"director", "Leonid Gaidai"},
-            {"producer", "Peter Feller"},
-            {"actorAndRole", {{"Evgeny Morgunov", "Experienced"}, {"George Vitsin", "Coward"}, {"Yuri Nikulin", "Idiot"}}}
+int main() {
+    nlohmann::json j = {
+            {"Moonshiners", {
+                {"country", "USSR"},
+                {"date", "1962"},
+                {"studio", "Mosfilm"},
+                {"scriptwriter", "Konstantin Brovin, Leonid Gaidai"},
+                {"director", "Leonid Gaidai"},
+                {"producer", "Peter Feller"},
+                {"actorAndRole", {{"Evgeny Morgunov", "Experienced"},
+                                  {"George Vitsin", "Coward"},
+                                  {"Yuri Nikulin", "Idiot"}}}
+            }}};
 
-    };
-
-    std::ofstream out("./film.json");
-    out << std::setw(4) << moonshiners;
+    std::ofstream out("film.json");
+    out << std::setw(4) << j;
     out.close();
 
-    std::cout << moonshiners.dump(4);
+    j.clear();
 
-    auto v8 = moonshiners["actorAndRole"].get<std::map<std::string, nlohmann::json>>();
+    std::ifstream in("film.json");
 
-    std::map<std::string, std::vector<std::map<std::string, std::string>>> maps;
-
-    for (auto &i : v8)
+    if (!in.is_open()) std::cout << "File not found!";
+    else
     {
-        m_new[i.first] = i.second.get<std::vector<int>>();
+        in >> j;
+        in.close();
+
+        std::cout << j.dump(4);
     }
-
-
-        maps = v8.get<std::vector<std::map<std::string, std::string>>>();
-
-
-    Moonshiners m;
-
-std::cout << "QQQ";
-
-
-
-
-
 }
